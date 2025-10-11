afa18f4b66b2007c66c4760717c3b09635867174
Zeydrk <zedricveran7@gmail.com>
2025-10-11 23:12:10 +0800

    imdedagain

--- index.js
+++ index.js
@@ -4,60 +4,67 @@ const cors = require('cors');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const session = require('express-session');
-
-// importing routes
-// const userRoutes = require('./src/User/user-routes')
-
 const app = express();
-
-// setting up localStrategy
-
+// Setting up global variable for user, dunno if it will work
+let user = [{
+  username: "user",
+  password: "pass"
+}]
+// setting up middlewares
+app.use(cors());
+app.use(express.json());
+// Initializing passport
+// Establishing sessions in express
 app.use(session({
   secret: 'supersecretkey',
   resave: false,
   saveUninitialized: true,
 }))
-app.use(passport.session())
+// Establishing sessions in passport
 app.use(passport.initialize())
-app.use(cors());
-app.use(express.json());
-// Serializing za user
-passport.serializeUser((user, done) =>{
-  done(null,user.username)
+app.use(passport.session())
+// Serialization
+passport.serializeUser((user, done) => {
+  done(null, user.username)
 })
-
-// deserializing za user
-passport.deserializeUser((username, done) =>{
-  if (username){
-    done(null, user)
+// Deserialization
+passport.deserializeUser((username, done)=>{
+  const result = user.find((user) => user.username == username)
+  if(result){
+    done(null,result)
   }
   else{
-    done(err, null)
+    done(false,null)
   }
 })
-
-new LocalStrategy((username, password, done) => {
-  if (err) {
-    done(err, null);
-  };
-  if (user !== null) {
-    done(null, user);
+// Usage of the local strategy
+app.post('/login', passport.authenticate('local'),(req,res) =>{
+  res.send(user)
+})
+// Using localStrategy middleware
+passport.use(new LocalStrategy((username, password, done) =>{
+  // console.log(username)
+  // console.log(password)
+  const results = user.find((user) => user.username == username)
+  // console.log(results)
+  if(results != null) {
+    done(null,results)
+  }
+  else{
+    done(false,null)
   }
 
-});
-passport.use(LocalStrategy)
-
-
+}))
 
 // to make backend not empty for testing
 app.get('/', (req, res) => {
   res.send("Working")
 })
 
-app.post('/login', passport.authenticate('local'),(req,res) =>{
-  const {username,password} = req.body
-  res.send({username,password})
+app.get('/users', (req, res) => {
+  res.send(user)
 })
+
 
 
 
